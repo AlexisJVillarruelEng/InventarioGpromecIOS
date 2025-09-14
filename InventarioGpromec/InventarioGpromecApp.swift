ac0d5af84b14bb6bc501bbdaf215cf79e13dc4f6
AlexisJVillarruelEng <alexisvillarruelj14@gmail.com>
2025-09-14 12:46:40 -0500

    Initial Commit

---
+++ InventarioGpromec/InventarioGpromecApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventarioGpromecApp.swift
+//  InventarioGpromec
+//
+//  Created by Alexis Villarruel on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct InventarioGpromecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
